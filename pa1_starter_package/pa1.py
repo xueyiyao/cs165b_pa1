6c736d7813f1bb0033b52c6b07a5a0f8b055f553
Xueyi (Allen) Yao <xueyiyao@umail.ucsb.edu>
2020-04-19 19:04:49 -0700

    Initial Commit

---
+++ pa1_starter_package/pa1.py
@@ -0,0 +1,32 @@
+# Starter code for CS 165B HW2 Spring 2019
+
+def run_train_test(training_input, testing_input):
+    """
+    Implement the training and testing procedure here. You are permitted
+    to use additional functions but DO NOT change this function definition.
+    You are permitted to use the numpy library but you must write
+    your own code for the linear classifier.
+
+    Inputs:
+        training_input: list form of the training file
+            e.g. [[3, 5, 5, 5],[.3, .1, .4],[.3, .2, .1]...]
+        testing_input: list form of the testing file
+
+    Output:
+        Dictionary of result values
+
+        IMPORTANT: YOU MUST USE THE SAME DICTIONARY KEYS SPECIFIED
+
+        Example:
+            return {
+                "tpr": #your_true_positive_rate,
+                "fpr": #your_false_positive_rate,
+                "error_rate": #your_error_rate,
+                "accuracy": #your_accuracy,
+                "precision": #your_precision
+            }
+    """
+
+
+    # TODO: IMPLEMENT
+    pass
